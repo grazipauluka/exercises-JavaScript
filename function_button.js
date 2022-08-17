76082bcc240f8e642952459ecf1e7a520619ca35
grazipauluka <grazipauluka@gmail.com>
2022-08-17 10:53:18 +0200

    add files

---
+++ function_button.js
@@ -0,0 +1,9 @@
+function handleButtonClick1(color) { 
+if (document.body.style.backgroundColor === color)
+  { document.body.style.backgroundColor = "white"
+    
+  } else {
+    document.body.style.backgroundColor = color;
+  }
+}
+      
